0f4027e9a2e4cd65055e40c565084e8c28406d35
Haru-KY <harutoon424@gmail.com>
2025-05-16 12:22:21 +0900

    usersデータベースとの接続を行うusers.jsファイルの作成

---
+++ amsac/db/users.js
@@ -0,0 +1,6 @@
+import knex from "knex";
+import config from "../knexfile.js";
+
+const users_db = knex(config.users);
+
+export default users_db;
--- amsac/routes/index.js
+++ amsac/routes/index.js
@@ -2,6 +2,8 @@ import mysql from "mysql2";
 import express from 'express';
 const router = express.Router();
 
+import users from "../db/users.js";
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
