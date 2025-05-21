f8135911ae86fdcb651fffe2aa356984c4c2c82d
Haru-KY <harutoon424@gmail.com>
2025-05-21 21:27:43 +0900

    tagおよびaitagの表示機能、すべてのメールの表示機能の追加

--- amsac/routes/add.js
+++ amsac/routes/add.js
@@ -15,12 +15,84 @@ router.get( '/', async function ( req, res, next) {
 
         }
 
+        const tagName = req.query.tag;
+        const aiTagName = req.query.aitag;
 
-        const emails = await knex( "email" )
+        let emailsQuery = knex("email")
             .select("id", "subject", "body")
             .where("user_id", userId)
-            .orderBy("created_at", "desc")
-            .limit(10);
+
+        if (tagName && tagName.trim() !== ""){
+
+            const tag = await knex("tags")
+                .select("id")
+                .where({ name: tagName, user_id: userId })
+                .first();
+
+            if (tag) {
+
+                const emailIds = await knex("email_tags")
+                    .pluck("email_id")
+                    .where({ tag_id: tag.id, user_id: userId });
+
+                if(emailIds.length > 0)
+                {
+
+                    emailsQuery.whereIn("id", emailIds);
+
+                }else{
+
+                    emailsQuery.whereRaw('0=1');
+
+                }
+
+            } else {
+                // タグが存在しない場合は空の結果にする
+                emailsQuery.whereRaw('0 = 1');
+            }
+
+        }
+
+        if(aiTagName && aiTagName.trim() !== ""){
+
+            const aitag = await knex("ai_tags")
+                .select("id")
+                .where({ name: aiTagName, user_id: userId })
+                .first();
+
+            if(aitag){
+
+                const emailIds = await knex("email_ai_tags")
+                    .pluck("email_id")
+                    .where({ tag_id: aitag.id, user_id: userId });
+
+                if (emailIds.length > 0) {
+
+                    emailsQuery.whereIn("id", emailIds);
+
+                } else {
+
+                    emailsQuery.whereRaw('0 = 1');
+
+                }
+
+            } else {
+
+                
+                emailsQuery.whereRaw('0=1');
+
+            }
+
+        }
+
+        emailsQuery = emailsQuery.orderBy( "created_at", "desc" );
+
+        const emali_tags = await knex( "email_tags")
+            .select("email_id", "tag_id")
+            .where("user_id", userId)
+
+
+        const emails = await emailsQuery;
 
         const tags = await knex( "tags" )
             .select("id", "user_id", "name")
@@ -34,7 +106,8 @@ router.get( '/', async function ( req, res, next) {
             title: "Main page",
             emails: emails,
             tags: tags,
-            ai_tags: ai_tags
+            ai_tags: ai_tags,
+            emial_tags: emali_tags
         });
     } catch (err) {
 
